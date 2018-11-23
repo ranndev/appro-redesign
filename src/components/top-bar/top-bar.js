95416b7703594afc38236521420c3a3c6b677597
Rannie <rannieperalta092796@gmail.com>
2018-11-24 06:57:39 +0800

    Set topbar collapsed as initial state

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -1,21 +1,22 @@
 import template from './top-bar.html';
 
 class Controller {
-	constructor($element, NavigationStore) {
-		this.locations = NavigationStore.getState().locations;
+  constructor($element, NavigationStore) {
+    this.collapsed = true;
+    this.locations = NavigationStore.getState().locations;
 
-		NavigationStore.getStateStream('TOGGLE_FLOAT_HEADER', ({isFloatHeader}) => {
-			isFloatHeader ? $element.addClass('float') : $element.removeClass('float');
-		});
-	}
+    NavigationStore.getStateStream('TOGGLE_FLOAT_HEADER', ({isFloatHeader}) => {
+      isFloatHeader ? $element.addClass('float') : $element.removeClass('float');
+    });
+  }
 }
 
 export default {
-	bindings: {
-		brandName: '@',
-		brandUrl: '@',
-	},
-	template: template,
-	controller: Controller,
-	controllerAs: 'topBar',
+  bindings: {
+    brandName: '@',
+    brandUrl: '@',
+  },
+  template: template,
+  controller: Controller,
+  controllerAs: 'topBar',
 };
