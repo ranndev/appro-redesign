88f5ed16ee5426852117a5f9686ded9c2d3a8be0
Rannie <rannie-peralta@users.noreply.github.com>
2018-11-24 05:21:14 +0800

    Separate section components (#3)

--- src/app.script.js
+++ src/app.script.js
@@ -3,11 +3,13 @@ import './app.vendor';
 import './models/_index';
 import './stores/_index';
 import './components/_index';
+import './sections/_index';
 
 angular.module('app', [
 	'app.models',
 	'app.stores',
 	'app.components',
+	'app.sections',
 ]).run((NavigationStore) => {
 	window.onload = () => {
 		document.querySelector('loader').classList.add('hidden');
