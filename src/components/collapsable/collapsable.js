f9c3dc9fa2bc5e86c2add6f50a79518ea8849f83
Rannie <rannieperalta092796@gmail.com>
2018-11-24 13:45:15 +0800

    Make section-faq component responsive

--- src/components/collapsable/collapsable.js
+++ src/components/collapsable/collapsable.js
@@ -1,48 +1,53 @@
 import template from './collapsable.html';
 
 class Controller {
-	constructor($scope, $element, $timeout) {
-		this.$element = $element;
-		this.$timeout = $timeout;
-		this.$content = angular.element($element[0].lastElementChild);
+  constructor($scope, $element, $timeout) {
+    this.$element = $element;
+    this.$timeout = $timeout;
+    this.$content = angular.element($element[0].lastElementChild);
 
-		$scope.$watch('collapsable.collapsed', () => {
-			this.initialized && this.update();
-		});
-	}
+    $scope.$watch('collapsable.collapsed', () => {
+      this.initialized && this.update();
+    });
+  }
 
-	$onInit() {
-		this.initialized = true;
+  $onInit() {
+    this.initialized = true;
 
-		this.$timeout(() => {
-			this.update();
-		}, 1000);
-	}
+    this.$timeout(() => {
+      this.update();
+    });
+  }
 
-	update() {
-		if (this.collapsed) {
-			this.$content.css('margin-top', `-${this.$content[0].offsetHeight}px`);
-			this.$element.addClass('collapsed');
-		} else {
-			this.$content.css('margin-top', null);
-			this.$element.removeClass('collapsed');
-		}
-	}
+  update() {
+    if (this.collapsed) {
+      this.$content.css('margin-top', `-${this.$content[0].offsetHeight}px`);
+      this.$element.addClass('collapsed');
 
-	toggle() {
-		this.collapsed = !this.collapsed;
-		this.onToggle({collapsed: this.collapsed});
-	}
+      this.$timeout(() => {
+        this.$content.css('height', 0);
+      }, 300);
+    } else {
+      this.$content.css('height', null);
+      this.$content.css('margin-top', null);
+      this.$element.removeClass('collapsed');
+    }
+  }
+
+  toggle() {
+    this.collapsed = !this.collapsed;
+    this.onToggle({collapsed: this.collapsed});
+  }
 }
 
 export default {
-	template: template,
-	bindings: {
-		collapsed: '=',
-		heading: '@',
-		content: '@',
-		onToggle: '&',
-	},
-	controller: Controller,
-	controllerAs: 'collapsable',
+  template: template,
+  bindings: {
+    collapsed: '=',
+    heading: '@',
+    content: '@',
+    onToggle: '&',
+  },
+  controller: Controller,
+  controllerAs: 'collapsable',
 };
