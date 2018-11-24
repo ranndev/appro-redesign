14aba831567cb22066d4f1b1f4cb110a9713837e
Rannie <rannieperalta092796@gmail.com>
2018-11-24 15:34:02 +0800

    Make section-contact responsive

--- src/sections/section-contact/section-contact.js
+++ src/sections/section-contact/section-contact.js
@@ -3,7 +3,7 @@ import template from './section-contact.html';
 class Controller {}
 
 export default {
-	template: template,
-	controller: Controller,
-	controllerAs: 'section',
+  template: template,
+  controller: Controller,
+  controllerAs: 'section',
 };
