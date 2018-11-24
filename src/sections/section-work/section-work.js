b1addb625ef2bd23e182878778e56e587e80209d
Rannie <rannieperalta092796@gmail.com>
2018-11-24 09:10:19 +0800

    Make section-works-component responsive

--- src/sections/section-work/section-work.js
+++ src/sections/section-work/section-work.js
@@ -1,27 +1,27 @@
 import template from './section-work.html';
 
 const defaultCard = {
-	title: '',
-	description: 'Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident',
+  title: '',
+  description: 'Lorem ipsum dolor sit ameteped consecteadop adipisicing elitab sed eiusmod temporara incident',
 };
 
 class Controller {
-	constructor() {
-		this.cards = [
-			{title: 'Login First'},
-			{title: 'Data Analysis'},
-			{title: 'Face Testing'},
-			{title: 'Show Result'},
-		];
+  constructor() {
+    this.cards = [
+      {title: 'Login First'},
+      {title: 'Data Analysis'},
+      {title: 'Face Testing'},
+      {title: 'Show Result'},
+    ];
 
-		this.cards = this.cards.map((card, index) => {
-			return Object.assign({}, defaultCard, card, {id: index + 1});
-		});
-	}
+    this.cards = this.cards.map((card, index) => {
+      return Object.assign({}, defaultCard, card, {id: index + 1});
+    });
+  }
 }
 
 export default {
-	template: template,
-	controller: Controller,
-	controllerAs: 'work',
+  template: template,
+  controller: Controller,
+  controllerAs: 'work',
 };
