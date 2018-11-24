0a88d13e121b6a76b63189ec2567b4012caedbd7
Rannie <rannie-peralta@users.noreply.github.com>
2018-11-24 11:01:40 +0800

    Make section-team component responsive (#9)

--- src/sections/section-team/section-team.js
+++ src/sections/section-team/section-team.js
@@ -1,31 +1,31 @@
 import template from './section-team.html';
 
 class Controller {
-	constructor() {
-		this.teamImageUrlFormat = (id) => `assets/images/team/team${id}.jpg`;
+  constructor() {
+    this.teamImageUrlFormat = (id) => `assets/images/team/team${id}.jpg`;
 
-		this.teams = [{
-			name: 'Jemy Sedonce',
-			position: 'Co. Founder',
-		}, {
-			name: 'Deborah Brown',
-			position: 'UX Designer',
-		}, {
-			name: 'Harry Franks',
-			position: 'Founder',
-		}, {
-			name: 'Victoria Clark',
-			position: 'Creative Director',
-		}];
+    this.teams = [{
+      name: 'Jemy Sedonce',
+      position: 'Co. Founder',
+    }, {
+      name: 'Deborah Brown',
+      position: 'UX Designer',
+    }, {
+      name: 'Harry Franks',
+      position: 'Founder',
+    }, {
+      name: 'Victoria Clark',
+      position: 'Creative Director',
+    }];
 
-		this.teams = this.teams.map((team, index) => {
-			return Object.assign(team, {url: this.teamImageUrlFormat(index+1)});
-		});
-	}
+    this.teams = this.teams.map((team, index) => {
+      return Object.assign(team, {url: this.teamImageUrlFormat(index+1)});
+    });
+  }
 }
 
 export default {
-	template: template,
-	controller: Controller,
-	controllerAs: 'section',
+  template: template,
+  controller: Controller,
+  controllerAs: 'section',
 };
