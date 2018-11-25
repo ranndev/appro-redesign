0bea5d759d2cb1057de183fd695938297751109c
Rannie <rannie-peralta@users.noreply.github.com>
2018-11-25 23:20:43 +0800

    Add auto-scroll-top component (#17)

--- src/components/_index.js
+++ src/components/_index.js
@@ -1,9 +1,11 @@
+import autoScrollTop from './auto-scroll-top/auto-scroll-top';
 import loader from './loader/loader';
 import topBar from './top-bar/top-bar';
 import collapsable from './collapsable/collapsable';
 
 angular
 	.module('app.components', [])
+  .component('autoScrollTop', autoScrollTop)
 	.component('loader', loader)
 	.component('topBar', topBar)
 	.component('collapsable', collapsable);
